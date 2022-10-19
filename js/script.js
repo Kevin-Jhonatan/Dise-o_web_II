d2b95b98f9f43eca495ef0e0e53cc19d78267eec
keevj <jhonatan72700606@gmail.com>
2022-10-19 11:33:14 -0400

    Actualizacion

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
+/* const canvas = document.querySelector("canvas");
+const rombo = document.createElement("div");
+rombo.id = "rombo";
+canvas.appendChild(rombo);
+const divRombo = document.getElementById("rombo");
+divRombo.classList.toggle("rombos"); */
 /* Efectos de scroll */
 ScrollReveal().reveal("#particles-js", { delay: 500 });
