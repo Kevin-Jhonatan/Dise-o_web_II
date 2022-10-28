9a999a14993a71310e05daa0918e8a14ddbc3f89
keevj <jhonatan72700606@gmail.com>
2022-10-27 23:35:47 -0400

    Update terminado desktop

--- js/script.js
+++ js/script.js
@@ -5,4 +5,9 @@ canvas.appendChild(rombo);
 const divRombo = document.getElementById("rombo");
 divRombo.classList.toggle("rombos"); */
 /* Efectos de scroll */
+ScrollReveal().reveal(".header", { delay: 100 });
 ScrollReveal().reveal("#particles-js", { delay: 500 });
+ScrollReveal().reveal("#sobre-nosotros", { delay: 700 });
+ScrollReveal().reveal("#productos", { delay: 700 });
+ScrollReveal().reveal("#sucursales", { delay: 700 });
+ScrollReveal().reveal(".contact-us", { delay: 800 });
