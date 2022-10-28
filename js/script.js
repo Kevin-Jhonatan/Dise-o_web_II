8f8fe985e1f3c3f67631c6db18d6b9e72f447605
keevj <jhonatan72700606@gmail.com>
2022-10-27 23:38:12 -0400

    Update js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,8 @@
-/* const canvas = document.querySelector("canvas");
-const rombo = document.createElement("div");
-rombo.id = "rombo";
-canvas.appendChild(rombo);
-const divRombo = document.getElementById("rombo");
-divRombo.classList.toggle("rombos"); */
 /* Efectos de scroll */
 ScrollReveal().reveal(".header", { delay: 100 });
-ScrollReveal().reveal("#particles-js", { delay: 500 });
-ScrollReveal().reveal("#sobre-nosotros", { delay: 700 });
-ScrollReveal().reveal("#productos", { delay: 700 });
-ScrollReveal().reveal("#sucursales", { delay: 700 });
-ScrollReveal().reveal(".contact-us", { delay: 800 });
+ScrollReveal().reveal("#particles-js", { delay: 100 });
+ScrollReveal().reveal("#sobre-nosotros", { delay: 1000 });
+ScrollReveal().reveal("#productos", { delay: 1000 });
+ScrollReveal().reveal("#sucursales", { delay: 1000 });
+ScrollReveal().reveal(".contact-us", { delay: 1000 });
+ScrollReveal().reveal(".footer", { delay: 1000 });
