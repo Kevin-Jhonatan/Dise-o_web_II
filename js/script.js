e38d09e6b94c412a349b309cca5083e5010fe866
keevj <jhonatan72700606@gmail.com>
2022-10-28 00:09:48 -0400

    Update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 /* Efectos de scroll */
 ScrollReveal().reveal(".header", { delay: 100 });
 ScrollReveal().reveal("#particles-js", { delay: 100 });
-ScrollReveal().reveal("#sobre-nosotros", { delay: 1000 });
-ScrollReveal().reveal("#productos", { delay: 1000 });
-ScrollReveal().reveal("#sucursales", { delay: 1000 });
-ScrollReveal().reveal(".contact-us", { delay: 1000 });
-ScrollReveal().reveal(".footer", { delay: 1000 });
+ScrollReveal().reveal("#sobre-nosotros", { delay: 500 });
+ScrollReveal().reveal("#productos", { delay: 500 });
+ScrollReveal().reveal("#sucursales", { delay: 500 });
+ScrollReveal().reveal(".contact-us", { delay: 500 });
+ScrollReveal().reveal(".footer", { delay: 500 });
